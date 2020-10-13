4f81cce2245ba48366de81500dbad368b4fe1ff4
Ron Haber <haberrj95@gmail.com>
2020-10-13 16:11:56 +0200

    Changed the script to incorporate the mock trades

--- Real_Time/API_Utils/GetCurrencyPrices_rt.sh
+++ Real_Time/API_Utils/GetCurrencyPrices_rt.sh
@@ -15,3 +15,9 @@ sleep 0.1
 python3 /home/pi/CryptoCurrency/Real_Time/API_Utils/krakenapi.py Ticker pair=ltceur
 sleep 0.1
 
+python3 /home/pi/CryptoCurrency/Real_Time/run_investments.py -i BTC -p 0.0
+sleep 0.1
+python3 /home/pi/CryptoCurrency/Real_Time/run_investments.py -i ETH -p 0.0
+sleep 0.1
+python3 /home/pi/CryptoCurrency/Real_Time/run_investments.py -i LTC -p 0.0
+sleep 0.1
