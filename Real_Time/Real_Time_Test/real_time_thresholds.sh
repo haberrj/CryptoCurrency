697d1bb02518f12e7a8181ba1ad2dc679df0ab9c
Ron Haber <haberrj95@gmail.com>
2020-10-23 15:17:19 +0200

    Changed thresholds to be more runnable

--- Real_Time/Real_Time_Test/real_time_thresholds.sh
+++ Real_Time/Real_Time_Test/real_time_thresholds.sh
@@ -8,7 +8,7 @@
 cd /home/pi/CryptoCurrency/
 git pull
 
-python3 /home/pi/CryptoCurrency/Real_Time/Real_Time_Test/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i BTC -t 50 30 30 50 -p 0.00 &
+python3 /home/pi/CryptoCurrency/Real_Time/Real_Time_Test/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i BTC -t 30 30 30 30 -p 0.00 &
 P1=$!
 python3 /home/pi/CryptoCurrency/Real_Time/Real_Time_Test/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i LTC -t 30 30 30 30 -p 0.00 &
 P2=$!
