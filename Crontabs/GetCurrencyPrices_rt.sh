da10669e012464cb539e60aaf3642745bf05a2fc
Ron Haber <haberrj95@gmail.com>
2020-11-01 17:28:11 +0100

    Cleaned up Kraken scripts

--- Crontabs/GetCurrencyPrices_rt.sh
+++ Crontabs/GetCurrencyPrices_rt.sh
@@ -12,6 +12,6 @@ python3 /home/pi/CryptoCurrency/API_Utils/krakenapi.py Ticker pair=xbteur
 python3 /home/pi/CryptoCurrency/API_Utils/krakenapi.py Ticker pair=etheur
 python3 /home/pi/CryptoCurrency/API_Utils/krakenapi.py Ticker pair=ltceur
 
-python3 /home/pi/CryptoCurrency/Real_Time/run_investments.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -i BTC -p 0.0026
-python3 /home/pi/CryptoCurrency/Real_Time/run_investments.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -i ETH -p 0.0026
-python3 /home/pi/CryptoCurrency/Real_Time/run_investments.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -i LTC -p 0.0026
+python3 /home/pi/CryptoCurrency/Kraken/Real_Time/run_investments.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -i BTC -p 0.0026
+python3 /home/pi/CryptoCurrency/Kraken/Real_Time/run_investments.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -i ETH -p 0.0026
+python3 /home/pi/CryptoCurrency/Kraken/Real_Time/run_investments.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -i LTC -p 0.0026
--- Crontabs/real_time_thresholds.sh
+++ Crontabs/real_time_thresholds.sh
@@ -8,10 +8,10 @@
 cd /home/pi/CryptoCurrency/
 git pull
 
-python3 /home/pi/CryptoCurrency/Test_Thresholds/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i BTC -t 15 15 15 15 -p 0.0026 &
+python3 /home/pi/CryptoCurrency/Kraken/Test_Thresholds/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i BTC -t 15 15 15 15 -p 0.0026 &
 P1=$!
-python3 /home/pi/CryptoCurrency/Test_Thresholds/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i LTC -t 15 15 15 15 -p 0.0026 &
+python3 /home/pi/CryptoCurrency/Kraken/Test_Thresholds/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i LTC -t 15 15 15 15 -p 0.0026 &
 P2=$!
-python3 /home/pi/CryptoCurrency/Test_Thresholds/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i ETH -t 15 15 15 15 -p 0.0026 &
+python3 /home/pi/CryptoCurrency/Kraken/Test_Thresholds/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i ETH -t 15 15 15 15 -p 0.0026 &
 P3=$!
 wait $P1 $P2 $P3
