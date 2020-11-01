38ed03f5385ca397840ce19ba59559f6302f041b
Ron Haber <haberrj95@gmail.com>
2020-11-01 17:04:39 +0100

    Fixed bug with file system

--- Crontabs/binance_thresholds.sh
+++ Crontabs/binance_thresholds.sh
@@ -8,12 +8,12 @@
 cd /home/pi/CryptoCurrency/
 git pull
 
-python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i BTC -t 15 15 15 15 -p 0.00075 &
+python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Binance/ -c 1000.00 -i BTC -t 15 15 15 15 -p 0.00075 &
 P1=$!
-python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i ETH -t 15 15 15 15 -p 0.00075 &
+python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Binance/ -c 1000.00 -i ETH -t 15 15 15 15 -p 0.00075 &
 P2=$!
-python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i LINK -t 15 15 15 15 -p 0.00075 &
+python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Binance/ -c 1000.00 -i LINK -t 15 15 15 15 -p 0.00075 &
 P3=$!
-python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Real_Time_Artifacts/ -c 1000.00 -i BNB -t 15 15 15 15 -p 0.00075 &
+python3 /home/pi/CryptoCurrency/Binance/Algorithm/Backtest/threshold_executer.py -d /media/pi/HaberServer/Crypto_Share/Binance/ -c 1000.00 -i BNB -t 15 15 15 15 -p 0.00075 &
 P4=$!
 wait $P1 $P2 $P3 $P4
