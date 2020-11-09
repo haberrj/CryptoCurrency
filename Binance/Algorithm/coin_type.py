e6c5b865320580724976dfa46bdf83ab5991f8f3
Ron Haber <haberrj95@gmail.com>
2020-11-09 12:34:29 +0100

    Increased the margin of loss to 95%

--- Binance/Algorithm/coin_type.py
+++ Binance/Algorithm/coin_type.py
@@ -182,10 +182,10 @@ class Currency:
                 self.current_holding_price = self.current_price
         if(self.coin > 0):
             self.current_holding_price = self.GetCurrentHoldingPrice() # need to set this value otherwise it will be equal to zero and the whole thing will fail
-            if((self.current_price < (self.current_holding_price*0.90)) or (first_val[0] > self.thresholds[2] and second_val < self.thresholds[3])):
+            if((self.current_price < (self.current_holding_price*0.95)) or (first_val[0] > self.thresholds[2] and second_val < self.thresholds[3])):
                 # the addition of the holding price becoming too low will auto cause a sale of the asset itself
                 # this will prevent severe loss in the case of the underlying losing value
-                if((self.isProfitable(self.current_holding_price, self.current_price, self.commission)) or (self.current_price < (self.current_holding_price*0.90))):
+                if((self.isProfitable(self.current_holding_price, self.current_price, self.commission)) or (self.current_price < (self.current_holding_price*0.95))):
                     # This will only sell if the current holding is profitable or if there is a 0.5% drop in price
                     self.cash, paid = SellPercentageCurrency(self.coin, self.current_price, self.commission)
                     self.coin = 0
