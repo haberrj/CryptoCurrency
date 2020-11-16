5a94ea3d35adb5e1dece835d7f01ae7b819017fe
Ron Haber <haberrj95@gmail.com>
2020-11-16 09:02:41 +0100

    Commented out actual buy and sell for test purposes

--- Binance/bin_actual_investments.py
+++ Binance/bin_actual_investments.py
@@ -125,18 +125,18 @@ def ExecuteRealTime(client, data_direc, info, actual_cash):
             else:
                 quantity = "{:0.0{}f}".format(float(cash/new_price), 3)
             print("quantity: ", quantity)
-            order_info = client.BuyItem(name, quantity)
-            order_info["price"] = new_price
-            # order_info = client.TestOrder("buy", name, quantity)
-            # order_info = {
-            #     "time": "0",
-            #     "name": name,
-            #     "id": "0",
-            #     "price": new_price,
-            #     "status": "FILLED",
-            #     "coin": quantity,
-            #     "type": "BUY"
-            # }
+            # order_info = client.BuyItem(name, quantity)
+            # order_info["price"] = new_price
+            order_info = client.TestOrder("buy", name, quantity)
+            order_info = {
+                "time": "0",
+                "name": name,
+                "id": "0",
+                "price": new_price,
+                "status": "FILLED",
+                "coin": quantity,
+                "type": "BUY"
+            }
             if(order_info == False):
                 print("Insufficient Funds")
         elif(action == 2):
@@ -149,18 +149,18 @@ def ExecuteRealTime(client, data_direc, info, actual_cash):
             else:
                 quantity = "{:0.0{}f}".format(quantity, 3)
             print("quantity: ", quantity)
-            order_info = client.SellItem(name, quantity)
-            order_info["price"] = new_price
-            # order_info = client.TestOrder("sell", name, quantity)
-            # order_info = {
-            #     "time": "0",
-            #     "name": name,
-            #     "id": "0",
-            #     "price": new_price,
-            #     "status": "FILLED",
-            #     "coin": quantity,
-            #     "type": "SELL"
-            # }
+            # order_info = client.SellItem(name, quantity)
+            # order_info["price"] = new_price
+            order_info = client.TestOrder("sell", name, quantity)
+            order_info = {
+                "time": "0",
+                "name": name,
+                "id": "0",
+                "price": new_price,
+                "status": "FILLED",
+                "coin": quantity,
+                "type": "SELL"
+            }
             if(order_info == False):
                 print("Error with order")
         order_info_array.append(order_info)
