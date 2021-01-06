35aa8cd82c8776b26d4db790bc7f195bc4b4c2fa
Ron Haber <haberrj95@gmail.com>
2021-01-06 13:24:20 +0100

    Fixed file locations

--- Crontabs/binance_other_coins.sh
+++ Crontabs/binance_other_coins.sh
@@ -3,8 +3,8 @@
 # Date: 31.10.2020
 # This script will run all of the different collections for other coins
 
-cd /home/pi/CryptoCurrency/
+cd /root/CryptoCurrency/
 now=$(date)
 echo $now
 
-python3 /root/CryptoCurrency/Binance/collect_coin_info.py -d /root/Crypto_Docs/Binance/ -i ADA XRP SXP DOT LINK
+python3 /root/CryptoCurrency/Binance/collect_coin_info.py -d /root/Crypto_Share/Binance/ -i ADA XRP SXP DOT LINK
--- Crontabs/real_binance_trading.sh
+++ Crontabs/real_binance_trading.sh
@@ -8,32 +8,32 @@ now=$(date)
 echo $now
 git pull
 
-python3 /root/CryptoCurrency/Binance/bin_actual_investments.py -d /root/Crypto_Docs/Binance/ -i BTC ETH LTC -r 1
+python3 /root/CryptoCurrency/Binance/bin_actual_investments.py -d /root/Crypto_Share/Binance/ -i BTC ETH LTC -r 1
 
 # conversione to db files
 
 # Transaction Data
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/CSV_Transactions/BTC_transactions.csv -n BTC_Transactions
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/CSV_Transactions/ETH_transactions.csv -n ETH_Transactions
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/CSV_Transactions/LTC_transactions.csv -n LTC_Transactions
-# python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/CSV_Transactions/BNB_transactions.csv -n BNB_Transactions
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/CSV_Transactions/BTC_transactions.csv -n BTC_Transactions
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/CSV_Transactions/ETH_transactions.csv -n ETH_Transactions
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/CSV_Transactions/LTC_transactions.csv -n LTC_Transactions
+# python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/CSV_Transactions/BNB_transactions.csv -n BNB_Transactions
 # Price Data
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/BTC_Realtime.csv -n BTC_Realtime
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/ETH_Realtime.csv -n ETH_Realtime
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/LTC_Realtime.csv -n LTC_Realtime
-# python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/BNB_Realtime.csv -n BNB_Realtime
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/BTC_Realtime.csv -n BTC_Realtime
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/ETH_Realtime.csv -n ETH_Realtime
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/LTC_Realtime.csv -n LTC_Realtime
+# python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/BNB_Realtime.csv -n BNB_Realtime
 # Cash Data
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/Balances/BTC_Balance.csv -n BTC_Balance
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/Balances/ETH_Balance.csv -n ETH_Balance
-python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/Balances/LTC_Balance.csv -n LTC_Balance
-# python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Docs/Binance/Actual/Balances/BNB_Balance.csv -n BNB_Balance
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/Balances/BTC_Balance.csv -n BTC_Balance
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/Balances/ETH_Balance.csv -n ETH_Balance
+python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/Balances/LTC_Balance.csv -n LTC_Balance
+# python3 /root/CryptoCurrency/Utility/create_sqldb.py -c /root/Crypto_Share/Binance/Actual/Balances/BNB_Balance.csv -n BNB_Balance
 
 # Creation of graphs
 # Balances
-python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Docs/Binance/Actual/Balances/BTC_Balance.csv -v cash -t /root/Crypto_Docs/Binance/Databases/
-python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Docs/Binance/Actual/Balances/ETH_Balance.csv -v cash -t /root/Crypto_Docs/Binance/Databases/
-python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Docs/Binance/Actual/Balances/LTC_Balance.csv -v cash -t /root/Crypto_Docs/Binance/Databases/
+python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Share/Binance/Actual/Balances/BTC_Balance.csv -v cash -t /root/Crypto_Share/Binance/Databases/
+python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Share/Binance/Actual/Balances/ETH_Balance.csv -v cash -t /root/Crypto_Share/Binance/Databases/
+python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Share/Binance/Actual/Balances/LTC_Balance.csv -v cash -t /root/Crypto_Share/Binance/Databases/
 # Prices
-python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Docs/Binance/BTC_Realtime.csv -v price -t /root/Crypto_Docs/Binance/Databases/
-python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Docs/Binance/ETH_Realtime.csv -v price -t /root/Crypto_Docs/Binance/Databases/
-python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Docs/Binance/LTC_Realtime.csv -v price -t /root/Crypto_Docs/Binance/Databases/
+python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Share/Binance/BTC_Realtime.csv -v price -t /root/Crypto_Share/Binance/Databases/
+python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Share/Binance/ETH_Realtime.csv -v price -t /root/Crypto_Share/Binance/Databases/
+python3 /root/CryptoCurrency/Utility/graph_creation.py -c /root/Crypto_Share/Binance/LTC_Realtime.csv -v price -t /root/Crypto_Share/Binance/Databases/
